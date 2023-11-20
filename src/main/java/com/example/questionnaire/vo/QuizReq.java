6a154722f142a714257931a980a5d3ea8d7c7478
naginegi <2000vsteven@gmail.com>
2023-11-20 11:46:53 +0800

    test

---
+++ src/main/java/com/example/questionnaire/vo/QuizReq.java
@@ -0,0 +1,30 @@
+package com.example.questionnaire.vo;
+
+import java.util.List;
+
+import com.example.questionnaire.entity.Question;
+import com.example.questionnaire.entity.Questionnaire;
+
+//存前端來的東西  稱作"請求"
+public class QuizReq extends QuizVo{
+
+	public QuizReq() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public QuizReq(Questionnaire questionnaire, List<Question> questionList) {
+		super(questionnaire, questionList);
+		// TODO Auto-generated constructor stub
+	}
+
+	public QuizReq(Questionnaire questionnaire) {
+		super(questionnaire);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
+
+	
+}
