2fe5dafe44fa82f56cf2f0d3c6d913fe004c76b3
naginegi <2000vsteven@gmail.com>
2023-11-21 10:03:48 +0800

    create

--- src/main/java/com/example/questionnaire/controller/QuizController.java
+++ src/main/java/com/example/questionnaire/controller/QuizController.java
@@ -3,6 +3,7 @@ package com.example.questionnaire.controller;
 import java.time.LocalDate;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,30 +19,32 @@ import com.example.questionnaire.vo.QuizSearchReq;
 @RestController
 @CrossOrigin
 public class QuizController {
-	
+
 	@Autowired
 	private QuizService quizService;
-	
+
 	// 請求的方法
-	// ResquestBody 把外部傳進來的 JSON 格式 轉成class  ??
+	// ResquestBody 把外部傳進來的 JSON 格式 轉成class ??
 	// PostMapping 新增 post 提供 http methods
-	@PostMapping(value = "api/quiz/create")  
+	@PostMapping(value = "api/quiz/create")
 	public QuizRes create(@RequestBody QuizReq req) {
 		return quizService.create(req);
 	}
-/*
+
 	@GetMapping(value = "api/quiz/search")
-	public QuizRes search(@RequestBody  QuizSearchReq req) {
-		System.out.println(req.getTitle()+req.getStartDate()+req.getEndDate());
-		return quizService.search(req.getTitle(),req.getStartDate(),req.getEndDate());	
+	public QuizRes search(String title, @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
+			@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate) {
+		System.out.println(title + ":" + startDate + ":" + endDate);
+//		return null;
+		return quizService.search(title, startDate, endDate);
+	}
+	
+	/*
+	@GetMapping(value = "api/quiz/search")
+	public QuizRes search(@RequestBody QuizSearchReq req) {
+		System.out.println(req.getTitle()+":"+req.getStartDate()+":"+req.getEndDate());
+		return quizService.search(req.getTitle(),req.getStartDate(),req.getEndDate());
 	}
 	*/
-	@GetMapping(value = "api/quiz/search")
-	public QuizRes search(@RequestParam(name = "title", required = false) String title,
-	                      @RequestParam(name = "startDate", required = false) LocalDate startDate,
-	                      @RequestParam(name = "endDate", required = false) LocalDate endDate) {
-		
-	    return quizService.search(title, startDate, endDate);    
-	}
 
 }
--- src/main/java/com/example/questionnaire/vo/QuizSearchReq.java
+++ src/main/java/com/example/questionnaire/vo/QuizSearchReq.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class QuizSearchReq {
 
+	
 	private String title;
 	
 	@JsonProperty("start_date")
